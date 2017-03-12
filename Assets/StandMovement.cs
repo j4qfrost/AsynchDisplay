b0b5d32c2d2b6ed2b95f7730c88fee7f5c2f0c1d
AnhQuan Nguyen <j4qfrost@gmail.com>
2017-03-12 01:49:26 -0800

    first

---
+++ Assets/StandMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class StandMovement : MonoBehaviour {
+
+	int leftIndex = SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Leftmost);
+
+	SteamVR_Controller.Device leftDevice;
+	Vector2 leftAxis;
+
+	void Start ()
+	{
+		leftDevice = SteamVR_Controller.Input(leftIndex);
+		leftAxis  = leftDevice.GetAxis();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (leftDevice.GetPressDown(EVRButtonId.k_EButton_A))
+		{
+			GetComponentInParent<Transform> ().Translate (new Vector3(leftAxis.x,0,leftAxis.y));
+		}
+		else if (GetComponentInParent<Transform> ().localPosition != Vector3.zero) 
+		{
+			GetComponentInParent<Transform> ().localPosition = Vector3.zero;
+		}
+	}
+}
